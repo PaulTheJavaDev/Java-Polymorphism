5bad8685e9fc8de8600f50b6071ad486cbf314c9
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-20 16:07:00 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
+        AudiR6 audiR6 = new AudiR6();
         Boat boat = new Boat();
         Bike bike = new Bike();
 
-        Vehicle[] vehicles = {car, bike, boat};
+        Vehicle[] vehicles = {car, audiR6, bike, boat};
 
         for (Vehicle vehicle : vehicles) {
             vehicle.go();
